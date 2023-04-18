efd44e24f8d7daeac2cf5d1c1218cbfcb560c353
Miloxxxxx <131157416+Miloxxxxx@users.noreply.github.com>
2023-04-18 16:13:32 +0500

    Update adadada.py

--- adadada.py
+++ adadada.py
@@ -1,1 +1,37 @@
-Hello
+from PyQt5.QtWidgets import*
+
+app = QApplication([])
+okno = QWidget()
+
+main = QHBoxLayout()
+okno.setLayout(main)
+sub = QVBoxLayout()
+main.addLayout(sub)
+
+lap = QLineEdit()
+kno = QPushButton('Резудьтат')
+nadp = QLabel('Здесь будет результат')
+
+sub.addWidget(lap)
+sub.addWidget(kno)
+main.addWidget(nadp)
+
+def result():
+    a = lap.text()
+    try: 
+        a = int(a)
+        a += 2
+        a = str(a)
+        nadp.setText(a)
+    except:
+        okno2 = QMessageBox()
+        okno2.setText('нужно ввести число')
+        okno2.exec_()
+
+kno.clicked.connect(result)
+
+
+
+
+okno.show()
+app.exec_()
